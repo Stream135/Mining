f697527dd8135cd4f2f99668881724181a3d8a0e
Stream135 <110925850+Stream135@users.noreply.github.com>
2022-09-03 20:26:17 +0800

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz
+tar -xf lolMiner_v1.48_Lin64.tar.gz
+1.48/lolMiner --algo ETHASH --pool daggerhashimoto.eu-west.nicehash.com:3353 --user NHbXd2hEHrTTZyZcJVKC9NNNiPV3JRW5Ligc.Crypto --tls 0 --ethstratum ETHV1 pause
